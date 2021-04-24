f05c7e1aede934ab38228360bad0a51fa0e9f2dd
oyundalai2b <oyundalai2b@gmail.com>
2021-04-24 22:09:14 +1000

    add some script

--- frontend/main.js
+++ frontend/main.js
@@ -20,3 +20,42 @@
     );
   });
 })();
+
+//displayAllProducts() in index.html
+
+// const api_url = "http://localhost:3000/api/teddies";
+// async function getTeddies() {
+//   const response = await fetch(api_url);
+//   const data = await response.json();
+//   const { name, description, price } = data;
+
+//   document.getElementsByClassName("product-name").textContent = name;
+//   document.getElementsByClassName(
+//     "product-description"
+//   ).textContent = description;
+//   document.getElementsByClassName("product-price").textContent = price;
+// }
+// getTeddies();
+
+//showing imgages
+// console.log("fetch a image");
+// fetch("teddy_1.jpg")
+//   .then((response) => {
+//     console.log(response);
+//     return response.blob();
+//   })
+//   .then((blob) => {
+//     console.log(blob);
+//     document.getElementsByClassName("product-img").src = URL.createObjectURL(
+//       blob
+//     );
+//   });
+
+//showing images in another way
+// console.log("fatching an image");
+// catchImage();
+// async function catchImage() {
+//   const response = await fetch("teddy_1.jpg");
+//   const blob = await response.blob();
+//   document.getElementById("product-img").src = URL.createObjectURL(blob);
+// }
